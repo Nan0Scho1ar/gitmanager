a40716b80301593b8976d0734b1a783702508bd8
nan0scho1ar <scorch267@gmail.com>
2022-05-23 08:46:01 +1000

    Updated docstring

--- gitmanager.el
+++ gitmanager.el
@@ -183,12 +183,15 @@ awaited without blocking the main thread."
                     t)))
 
 (defun gitmanager-async-wait-for-buffer-test (buffer fn args)
-  "BUFFER FN ARGS."
   (if (null (with-current-buffer buffer (set-difference gitmanager-paths gitmanager-paths-completed)))
       (with-current-buffer buffer
         (apply fn args))
     ;; Tell async parent loop process to retry
     'gitmanager-loop-retry))
+  "Test if BUFFER is finished, then apply FN ARGS.
+If it is not finished return retry symbol.
+Do not call this directly, instead use
+gitmanager-async-wait-for-buffer-then-apply"
 
 
 ;; BEGIN gitmanager funcs
